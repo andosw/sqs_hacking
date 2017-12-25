2934c32759cbbaf2e71fc536b92892de5fe310c0
Sean Andersen <seana@squareup.com>
2017-12-25 09:35:05 -0800

    initial files

---
+++ src/main/java/com/seana/Main.java
@@ -0,0 +1,13 @@
+package com.seana;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+//        import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+    //import com.amazonaws.services.sqs.model.AmazonSQSException;
+    //import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+        System.out.println("Hello World");
+    }
+}
